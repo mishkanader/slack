6216bfb9691bf9a758e5cfcab120272ab549dfb4
Michele T <mishkanader@gmail.com>
2015-10-06 11:47:09 -1000

    lol

---
+++ .meteor/client/app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Mishkanader on 10/6/2015.
+ */
+Template.messages.helpers({
+  messages: [
+    { text: "All these messages" },
+    { text: "Uses the same template" },
+    { text: "But have a different data context" },
+    { text: "It's why these messages are all different!" },
+    { text: "Hey!" },
+    { text: "What's up man!" },
+    { text: "Hello" }
+  ]
+});
